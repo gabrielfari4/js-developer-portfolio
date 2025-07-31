82d4598f4a7a0ceaf99003b8aa7ec63c1bacd30a
gabrielfari4 <gabrielfaria88@gmail.com>
2025-07-31 15:58:02 -0300

    :sparkles: feat: api.js

---
+++ assets/js/api.js
@@ -0,0 +1,13 @@
+
+ const fetchProfileData = async () => {
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/gabrielfari4/js-developer-portfolio/refs/heads/projeto-base/data/profile.json')
+        const data = await response.json()
+         return data   
+    } catch (error) {
+        console.error(error)
+    }
+
+ }
+
+ fetchProfileData()
